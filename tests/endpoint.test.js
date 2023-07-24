72cc9eb82ec9ddc132026365ef1e04d9b15eb3c0
dasveloper <28981975+dasveloper@users.noreply.github.com>
2023-07-24 18:34:24 -0400

    Initial

---
+++ tests/endpoint.test.js
@@ -0,0 +1,31 @@
+import endpointHandler from "../pages/api/endpoint.js";
+import { createServer } from "http";
+import { apiResolver } from "next/dist/server/api-utils/node";
+import supertest from "supertest";
+
+const testClient = (handler) => {
+  const serverRequestListener = async (req, res) => {
+    return apiResolver(req, res, undefined, handler, {}, undefined);
+  };
+  const server = createServer(serverRequestListener);
+  return supertest(server);
+};
+
+describe("POST: /endpoint", () => {
+  it("Should post name and 1kb avatar", async () => {
+    await testClient(endpointHandler)
+      .post("/endpoint")
+      .field("name", "John Doe")
+      .attach("avater", Buffer.from("a".repeat(1000)), "filename")
+      .expect({ success: true })
+      .expect(200);
+  });
+  it("Should post name and 10mb avatar", async () => {
+    await testClient(endpointHandler)
+      .post("/endpoint")
+      .field("name", "John Doe")
+      .attach("avater", Buffer.from("a".repeat(10000000)), "filename")
+      .expect({ success: true })
+      .expect(200);
+  });
+});
